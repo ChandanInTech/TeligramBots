8c6ef475183090f180ba2d59691e49861b27222f
Chandan <kurella.chandu4@gmail.com>
2020-04-18 23:48:40 -0500

    Get chat ids by making an internet req

--- TeliBots/Test.py
+++ TeliBots/Test.py
@@ -1,5 +1,24 @@
-from TeliBots.CoronaCases import CoronaCases
+import json
 
-corona_cases = CoronaCases()
+import requests
 
-print(corona_cases.corona_sa_cases_update())
+token = "1240403795:AAHCKKtoNmrDWxOuxSxa4DgJD-S69PKhBLw"
+base = "https://api.telegram.org/bot{}/".format(token)
+
+
+def get_chat_ids():
+    url = base + "getUpdates"
+    r = requests.get(url)
+    resp = json.loads(r.content).get('result')
+
+    chat_ids = []
+
+    for x in resp:
+        chat_id = x.get('message').get('from').get('id')
+        if chat_id not in chat_ids:
+            chat_ids.append(chat_id)
+
+    return chat_ids
+
+
+print(get_chat_ids())
